5e3e45948626b66ad55b237b3462c72bb9d334fb
Aman Wairagkar <124350777+Amanw-25@users.noreply.github.com>
2025-01-10 23:22:55 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const BASE_URL="http://localhost:5000/api/v1";
+export const BASE_URL="https://med-ease.onrender.com/api/v1";
 export const token = localStorage.getItem("token")?.replace(/"/g, "");
 
