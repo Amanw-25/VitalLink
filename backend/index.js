97b6a309d383c53b745d12795274c19179277381
Aman Wairagkar <124350777+Amanw-25@users.noreply.github.com>
2025-01-10 23:29:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://med-ease-phi.vercel.app/',
+  origin: '*',
   methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Content-Type, Authorization',
 }));
